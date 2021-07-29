5909354cd02132ec5b00725d43a860662c615440
kavithaiii <87979350+kavithaiii@users.noreply.github.com>
2021-07-30 02:37:15 +0530

    add axios to access third part api
    - use dotenv-safe

--- server/controllers/wishlist-controller.js
+++ server/controllers/wishlist-controller.js
@@ -1,15 +1,29 @@
 // Import authorisation of api key and merchant id
+const axios = require('axios');
+require('dotenv-safe').config();
 
 // Retrieve all wishlist
 exports.wishlistAll = async (req, res) => {
   // Get all wishlist from backend
-  
-      
-      // Send a success message in response
+
+   console.log('api url>>>>', process.env.API_URL)
+   
+   console.log("'/wishlist/all' call");
+	  try {
+	    const res = await axios.get(`${process.env.API_URL}/storeadmin/me`,
+	    	{
+	    		headers: {
+			    'Authorization': `Basic ${process.env.Pid}:${process.env.API_KEY}`
+			  }
+	    	})
+	    // res.json(data)
+	    console.log('>>> response from SWYM relay')
+	    console.log(res.json(data))
+	  }
+	  catch (err) {
+	    console.log(err)
+	  }
+	  
+	// Send a success message in response
       res.status(200).json({ message: `Wishlist ... loading from server!` })
-    //})
-    //.catch(err => {
-      // Send status code and error message in response
-      //res.status(404).json({ message: `There was an error retrieving wishlist: ${err}` })
-   // })
 }
